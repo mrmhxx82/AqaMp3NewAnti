0255573a2d85179a1bc41840425f75b77847538d
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-04-11 13:25:13 +0430

    Update echo.lua

--- plugins/echo.lua
+++ plugins/echo.lua
@@ -14,7 +14,7 @@ return {
   description = "Reply Your Sent Message",
   usage = "/echo (message) : reply message",
   patterns = {
-    "^بگو +(.+)$"
+    "^!/echo +(.+)$"
   }, 
 	run = run,
 	moderated = true
